98ca1b68cafd62a2b64c7156880338e7943e62e2
hyperion4040 <hyperion1987@wp.pl>
2017-11-16 11:36:19 +0100

    Added serialization to json array of objects and connection to Azure sql.

--- Stypendium/Startup.cs
+++ Stypendium/Startup.cs
@@ -22,8 +22,8 @@ namespace Stypendium
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataAccess>(opt => opt.UseInMemoryDatabase());
-//           services.AddDbContext<DataAccess>(opt => opt.UseSqlServer(Configuration.GetSection("ConnectionStrings")["AzureConnection"]));
+//            services.AddDbContext<DataAccess>(opt => opt.UseInMemoryDatabase());
+           services.AddDbContext<DataAccess>(opt => opt.UseSqlServer(Configuration.GetSection("ConnectionStrings")["AzureConnection"]));
             
             services.AddMvc();
         }
